2b1b10095bce28701ff3965d2306c254aa0de5de
Pooja <pchandorkar@bestpeers.com>
2021-11-25 17:49:10 +0530

    My First rails project

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_11_23_072738) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
