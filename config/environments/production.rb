e8740cdf9c44a0153be50cdb1316260577c08502
Pooja <pchandorkar@bestpeers.com>
2021-12-02 15:57:22 +0530

    For heroku email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,21 +91,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
 
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.perform_deliveries = true
+  #config.action_mailer.delivery_method = :smtp
   host = 'localhost:3000'
   config.action_mailer.default_url_options = {host: host}
   config.action_mailer.smtp_settings  = {
   :address              => "smtp.gmail.com",
-  :port                 =>  587,
+  :port                 => 587,
   :user_name            => "poojachandorkar2552@gmail.com",
   :password             => "chandorkar@321",
   :authentication       => "plain",
   :enable_starttls_auto => true}
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+    #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    config.action_mailer.default_url_options = {:host => 'myapp.herokuapp.com', :protocol => 'http'}
 
   config.active_record.dump_schema_after_migration = false
 end
