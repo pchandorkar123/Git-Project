2d5b7f91355a230c177be7ec8b2057dece7f693a
Pooja <pchandorkar@bestpeers.com>
2021-12-02 16:42:14 +0530

    New email changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,11 +91,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
 
-  #config.action_mailer.raise_delivery_errors = true
-  #config.action_mailer.perform_deliveries = true
-  #config.action_mailer.delivery_method = :smtp
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = {host: host}
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  #host = 'localhost:3000'
+  config.action_mailer.default_url_options = {host: 'myapp.herokuapp.com'}
   config.action_mailer.smtp_settings  = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
@@ -105,7 +105,7 @@ Rails.application.configure do
   :enable_starttls_auto => true}
 
     #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-    config.action_mailer.default_url_options = {:host => 'myapp.herokuapp.com', :protocol => 'http'}
-
+    #onfig.action_mailer.default_url_options = {:host => 'myapp.herokuapp.com', :protocol => 'http'}
+    config.action_mailer.default_options = { from: 'contact@domain.com' }
   config.active_record.dump_schema_after_migration = false
 end
